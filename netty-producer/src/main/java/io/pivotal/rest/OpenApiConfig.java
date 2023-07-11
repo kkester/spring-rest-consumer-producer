4dabc389d7a0463cccbf3c6122e0188c5e7909a5
Keith Kester <kkester@vmware.com>
2023-07-11 11:53:47 -0500

    Added swagger docs to netty producer

---
+++ netty-producer/src/main/java/io/pivotal/rest/OpenApiConfig.java
@@ -0,0 +1,24 @@
+package io.pivotal.rest;
+
+import org.springdoc.core.GroupedOpenApi;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class OpenApiConfig {
+
+    @Bean
+    public GroupedOpenApi publicApi() {
+        return GroupedOpenApi.builder()
+            .group("springshop-public")
+            .pathsToMatch("/public/**")
+            .build();
+    }
+    @Bean
+    public GroupedOpenApi adminApi() {
+        return GroupedOpenApi.builder()
+            .group("springshop-admin")
+            .pathsToMatch("/admin/**")
+            .build();
+    }
+}
